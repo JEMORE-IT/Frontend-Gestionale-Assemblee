b55ad1960cc106c4b47fc6f7cf03a7f5eec345cf
gabbonj <gabriele030403@gmail.com>
2024-12-15 20:23:51 +0100

    moved login card in a separate component

--- app/components/atoms/components/login-page.tsx
+++ app/components/atoms/components/login-page.tsx
@@ -3,22 +3,14 @@
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { Button } from "@atoms/components/ui/button"
-import { Input } from "@atoms/components/ui/input"
-import { Card, CardContent } from "@atoms/components/ui/card"
 import { useRouter } from 'next/navigation'
 import axios from 'axios'
+import LoginCard from '@molecules/LoginCard'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export function LoginPage() {
   const router = useRouter()
-
-  const [loginerror, setLoginerror] = useState(false);
-  const [formData, setFormData] = useState({
-    username: '',
-    password: ''
-  })
 
   useEffect(() => {
     const verifyToken = async () => {
@@ -38,27 +30,6 @@ export function LoginPage() {
     verifyToken();
   }, []);
 
-  const handleSubmit = async  (e: React.FormEvent) => {
-    e.preventDefault()
-    try {
-      const response = await axios.post(`http://${API_BASE_URL}/authentication`, {
-        password: formData.password,
-      }, {
-        withCredentials: true,
-      });
-  
-      if (response.status === 200) {
-        router.push('/assemblee');
-      } else {
-        setLoginerror(true)
-        console.log('Errore di autenticazione');
-      }
-    } catch (error) {
-      setLoginerror(true)
-      console.log('Errore di rete');
-    }
-  }
-
   return (
     <main className="min-h-screen w-full bg-gradient-to-br from-[#3B44AC] to-[#2d3584] flex flex-col items-center justify-center p-4 relative">
       <div className="mb-8 w-full max-w-md">
@@ -77,61 +48,7 @@ export function LoginPage() {
         Gestionale Assemblee
       </h1>
       
-      <Card className="w-full max-w-md bg-white rounded-xl shadow-lg">
-        <CardContent className="p-6">
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div className="space-y-2">
-              <label 
-                htmlFor="username" 
-                className="block text-sm font-medium text-[#3B44AC]"
-                style={{ fontFamily: 'Gotham, sans-serif' }}
-              >
-                Username:
-              </label>
-              <Input
-                id="username"
-                type="text"
-                value={formData.username}
-                onChange={(e) => setFormData(prev => ({ ...prev, username: e.target.value }))}
-                className="w-full rounded-md border-[#3B44AC]"
-                onClick={() => {
-                  setLoginerror(false)
-                }}
-                required
-              />
-            </div>
-            
-            <div className="space-y-2">
-              <label 
-                htmlFor="password" 
-                className="block text-sm font-medium text-[#3B44AC]"
-                style={{ fontFamily: 'Gotham, sans-serif' }}
-              >
-                Password:
-              </label>
-              <Input
-                id="password"
-                type="password"
-                value={formData.password}
-                onChange={(e) => setFormData(prev => ({ ...prev, password: e.target.value }))}
-                className="w-full rounded-md border-[#3B44AC]"
-                onClick={() => {
-                  setLoginerror(false)
-                }}
-                required
-              />
-            </div>
-            {loginerror && <label className='text-red-600'>Username o password errati</label> }
-            <Button 
-              type="submit"
-              className="w-full mx-auto block bg-[#FFD241] hover:bg-[#3B44AC] text-[#3B44AC] hover:text-white transition-colors font-bold"
-              style={{ fontFamily: 'Gotham, sans-serif' }}
-            >
-              Login
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
+      <LoginCard/>
 
       <div className="absolute bottom-4 left-4">
         <Link 
---
+++ app/components/molecules/LoginCard/index.tsx
@@ -0,0 +1,100 @@
+import React, { FC, useState } from "react";
+import LoginCardProps from "./index.types"
+import { Button } from "@atoms/components/ui/button"
+import { Input } from "@atoms/components/ui/input"
+import { Card, CardContent } from "@atoms/components/ui/card"
+import axios from "axios";
+import { useRouter } from 'next/navigation'
+
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
+
+const LoginCard: FC<LoginCardProps> = ({ }) => {
+  const [loginerror, setLoginerror] = useState(false);
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+  
+  const router = useRouter()
+
+  const handleSubmit = async  (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      const response = await axios.post(`http://${API_BASE_URL}/authentication`, {
+        password: formData.password,
+      }, {
+        withCredentials: true,
+      });
+  
+      if (response.status === 200) {
+        router.push('/assemblee');
+      } else {
+        setLoginerror(true)
+        console.log('Errore di autenticazione');
+      }
+    } catch (error) {
+      setLoginerror(true)
+      console.log('Errore di rete');
+    }
+  }
+  
+  return <>
+    <Card className="w-full max-w-md bg-white rounded-xl shadow-lg">
+        <CardContent className="p-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
+            <div className="space-y-2">
+              <label 
+                htmlFor="username" 
+                className="block text-sm font-medium text-[#3B44AC]"
+                style={{ fontFamily: 'Gotham, sans-serif' }}
+              >
+                Username:
+              </label>
+              <Input
+                id="username"
+                type="text"
+                value={formData.username}
+                onChange={(e) => setFormData(prev => ({ ...prev, username: e.target.value }))}
+                className="w-full rounded-md border-[#3B44AC]"
+                onClick={() => {
+                  setLoginerror(false)
+                }}
+                required
+              />
+            </div>
+            
+            <div className="space-y-2">
+              <label 
+                htmlFor="password" 
+                className="block text-sm font-medium text-[#3B44AC]"
+                style={{ fontFamily: 'Gotham, sans-serif' }}
+              >
+                Password:
+              </label>
+              <Input
+                id="password"
+                type="password"
+                value={formData.password}
+                onChange={(e) => setFormData(prev => ({ ...prev, password: e.target.value }))}
+                className="w-full rounded-md border-[#3B44AC]"
+                onClick={() => {
+                  setLoginerror(false)
+                }}
+                required
+              />
+            </div>
+            {loginerror && <label className='text-red-600'>Username o password errati</label> }
+            <Button 
+              type="submit"
+              className="w-full mx-auto block bg-[#FFD241] hover:bg-[#3B44AC] text-[#3B44AC] hover:text-white transition-colors font-bold"
+              style={{ fontFamily: 'Gotham, sans-serif' }}
+            >
+              Login
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+  </>
+}
+
+export default LoginCard
