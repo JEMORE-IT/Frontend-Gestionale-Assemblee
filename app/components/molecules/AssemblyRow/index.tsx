f37cef779846b8ec46184a47d926dfa9ff5c4e46
gabbonj <gabriele030403@gmail.com>
2024-12-17 11:42:21 +0100

    added link to single assembly page

--- app/components/molecules/AssemblyRow/index.tsx
+++ app/components/molecules/AssemblyRow/index.tsx
@@ -1,12 +1,15 @@
 import React, { FC } from "react";
 import AssemblyRowProps from "./index.types"
 import { DeleteAssemblyDialog } from "@atoms/delete-assembly-dialog"
+import Link from 'next/link';
 
 const AssemblyRow: FC<AssemblyRowProps> = ({ assembly, onDelete }) => {
   return <>
     <div className="flex items-center justify-between rounded-lg border border-gray-200 bg-gray-50 p-4 shadow-sm">
       <span className="text-lg font-medium text-[#3B44AC]">
-        Assemblea {assembly.date}
+        <Link className="hover:text-gray-800" key={assembly.id} href={`/assemblee/assemblea/${assembly.id}`}>
+          Assemblea {assembly.date}
+        </Link>
       </span>
       <DeleteAssemblyDialog onDelete={() => onDelete(assembly.id)} />
     </div>
